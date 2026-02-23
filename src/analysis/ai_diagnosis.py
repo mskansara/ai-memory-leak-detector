a9a6529011c44895d86f29bdd791ef8b58a3ca0a
Manthan Kansara <manthankansara7@gmail.com>
2026-02-23 07:48:20 +0000

    Implemented llm using ollama

---
+++ src/analysis/ai_diagnosis.py
@@ -0,0 +1,42 @@
+import ollama
+import os
+from ollama import Client
+
+
+def diagnosis_leak(symbol_path):
+    client = Client(host="http://host.lima.internal:11434")
+    code_path = "./targets/target_app.cpp"
+
+    if not os.path.exists(code_path):
+        return "Source code not found."
+
+    with open(code_path, "r") as f:
+        source_code = f.read()
+
+    prompt = f"""
+    You are a Senior Systems Engineer. I have detected a memory leak using eBPF and Linear Regression.
+    
+    DETECTED LEAK PATH: {symbol_path}
+    
+    SOURCE CODE:
+    {source_code}
+    
+    TASK:
+    1. Identify the specific function in the source code causing the leak.
+    2. Explain WHY it is leaking.
+    3. Provide the corrected C++ code for that function.
+    """
+
+    print(f"🤖 AI Agent is analyzing the leak in: {symbol_path.split(';')[1]}...")
+
+    try:
+        response = client.generate(model="llama3.2", prompt=prompt)
+        print("\n--- AI DIAGNOSIS ---")
+        print(response["response"])
+    except Exception as e:
+        print(f"❌ Connection failed: {e}")
+
+
+if __name__ == "__main__":
+    test_path = "malloc;steady_leak;main"
+    diagnosis_leak(test_path)
